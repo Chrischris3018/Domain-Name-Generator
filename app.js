7597661c0a0cedf436f85a8bb4ec01a64fdbe3c4
Chrischris3018 <chrischrischris3018@yahoo.com>
2021-10-02 01:00:17 +0000

    working

--- app.js
+++ app.js
@@ -2,4 +2,16 @@ let pronoun = ['the','our'];
 let adj = ['great', 'big' ];
 let noun = ['jogger','racoon'];
 
-console.log ()
+function getRandomInt(length) {
+    return Math.floor(Math.random() * length);
+}
+
+const type1 = function(){
+    const pronoun1 = pronoun[ getRandomInt(pronoun.length ) ];
+    const adj2 = adj[getRandomInt(adj.length )];
+    const noun3 = noun[getRandomInt(noun.length )];
+
+    return `${pronoun1} ${adj2} ${noun3}`;
+}
+
+console.log(type1())
